a785051b5343006705a37dbf8de5c13daa7eb4c9
R7558 <rupsanaworld1004@gmail.co>
2023-05-27 14:25:12 +0600

    added routes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Home from '../components/Home/Home';
+import Category from '../components/Category/Category';
 
 const Routes = () => {
     return (
-        <div>
-            
-        </div>
+        <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/:categoryId" element={<Category />}></Route>
+    </Routes>
     );
 };
 
